e5b021e7672dba8fed45a059c76ca40b354798c2
Nurmukhametov, Eduard <nurmuedu@fel.cvut.cz>
2023-11-13 17:44:14 +0100

    test

---
+++ index.php
@@ -0,0 +1,23 @@
+<!-- Enable logs -->
+<?php
+error_reporting(E_ALL);
+error_reporting(-1);
+ini_set('error_reporting', E_ALL);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Todo List</title>
+</head>
+<body>
+	<h1>Welcome to Todo List</h1>
+	<h2>Todo List</h2>
+	<ul>
+		<li>Learn PHP</li>
+		<li>Learn Laravel</li>
+		<li>Learn Vue.js</li>
+	</ul>
+</body>
+</html>
