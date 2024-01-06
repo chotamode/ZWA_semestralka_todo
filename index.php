992290cb99267c874020a3eddbcebae42980c8af
Nurmukhametov, Eduard <nurmuedu@fel.cvut.cz>
2024-01-06 15:44:37 +0100

    Update CSS link in index.php

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
 	<head>
 		<meta charset="UTF-8">
 		<link rel="stylesheet" href="CSS/index.css">
-		<link rel="stylesheet" href="../CSS/topnav.css">
+		<link rel="stylesheet" href="CSS/topnav.css">
 		<title>Todo List</title>
 	</head>
 	<body>
